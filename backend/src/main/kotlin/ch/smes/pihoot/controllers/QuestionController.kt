fc7d96ba7a3eb9285b246920235df9161411cabe
Joël Bieli <joel.bieli@hispeed.ch>
2019-12-30 16:28:04 +0100

    Configure CORS policy to allow all origins/methods

--- backend/src/main/kotlin/ch/smes/pihoot/controllers/QuestionController.kt
+++ backend/src/main/kotlin/ch/smes/pihoot/controllers/QuestionController.kt
@@ -6,7 +6,6 @@ import ch.smes.pihoot.services.QuestionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/question")
 class QuestionController {
