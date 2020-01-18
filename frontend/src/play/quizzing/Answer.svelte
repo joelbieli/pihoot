7b341f7b7aca66a2e0c1c8e6fe620136240ce2d7
laniw <lani.wagner@edu.tbz.ch>
2020-01-18 13:52:17 +0100

    Adds question and answering page along with boilerplate for the scoreboard
    Other changes:
    Added TODOs for UI in player join screen
    Tweaked some UI parts in game loading screen
    Added a timed Loading bar that is used when displaying the question and then the answers.

---
+++ frontend/src/play/quizzing/Answer.svelte
@@ -0,0 +1,41 @@
+<script>
+	import TimedLoadingBar from '../../util/TimedLoadingBar.svelte';
+	import {getHexForColor} from '../../util/playUtils'
+
+	export let question;
+
+	let countDown;
+
+	let climbingI = 0;
+	for (let i = 15; i >= 0; i -= 1) {
+		setTimeout(() => countDown = i, 1000 * climbingI);
+		climbingI += 1;
+	}
+</script>
+
+<style>
+	.answer {
+		color: white;
+	}
+</style>
+
+<div class="uk-container uk-container-xsmall">
+	<TimedLoadingBar seconds="15" barColor="#1e87f0" wrapperColor="#bababa"/>
+	<h1>
+        {question.question}
+	</h1>
+	<div class="uk-grid-small" uk-grid>
+		<div class="uk-width-1-2 answer" style="background-color: {getHexForColor(question.answers[0].color)}">
+            {question.answers[0].answer}
+		</div>
+		<div class="uk-width-1-2 answer" style="background-color: {getHexForColor(question.answers[1].color)}">
+            {question.answers[1].answer}
+		</div>
+		<div class="uk-width-1-2 answer" style="background-color: {getHexForColor(question.answers[2].color)}">
+            {question.answers[2].answer}
+		</div>
+		<div class="uk-width-1-2 answer" style="background-color: {getHexForColor(question.answers[3].color)}">
+            {question.answers[3].answer}
+		</div>
+	</div>
+</div>
---
+++ frontend/src/play/quizzing/Question.svelte
@@ -0,0 +1,29 @@
+<script>
+	import TimedLoadingBar from '../../util/TimedLoadingBar.svelte';
+
+	export let question;
+	export let questionIndex;
+	export let questionCount;
+
+	let countDown;
+
+	let climbingI = 0;
+	for (let i = 5; i >= 0; i -= 1) {
+		setTimeout(() => countDown = i, 1000 * climbingI);
+		climbingI += 1;
+	}
+</script>
+
+<div class="uk-overlay uk-position-top-left">
+	{questionIndex+1} of {questionCount}
+</div>
+<div class="uk-container uk-container-xsmall">
+		<h2>
+			{question.question}
+		</h2>
+		<h3>
+            {countDown}
+		</h3>
+
+		<TimedLoadingBar seconds="5" barColor="#1e87f0" wrapperColor="#bababa"/>
+</div>
