7a79d0f1e14b6b9b61e4091797338b478be979ba
laniw <lani.wagner@edu.tbz.ch>
2020-01-19 16:01:25 +0100

    Adds scoreboard inbetween questions and adds clause for final scoreboard

---
+++ frontend/src/play/quizzing/Score.svelte
@@ -0,0 +1,31 @@
+<script>
+	import {getHexForColor, getTextHexForColor} from '../../util/playUtils'
+
+	export let color;
+	export let score;
+	export let position;
+
+	function fontBoldRule() {
+
+	}
+
+</script>
+
+<div class="uk-grid-small {position < 4 ? 'uk-padding-small' : ''} uk-border-rounded"
+     style="{(position < 4) ? `background-color: ${getHexForColor(color)}` : ''}" uk-grid>
+	<div class="uk-width-auto">
+		<span style="{(position < 4) ? `color: ${getTextHexForColor(color)}` : ''}">
+			{position}.
+		</span>
+	</div>
+	<div class="uk-width-expand">
+		<span style="{(position < 4) ? `font-weight: bold; color: ${getTextHexForColor(color)}` : ''}">
+			{color}
+		</span>
+	</div>
+	<div class="uk-width-auto">
+		<span style="{(position < 4) ? `font-weight: bold; color: ${getTextHexForColor(color)}` : ''}">
+			{score}
+		</span>
+	</div>
+</div>
--- frontend/src/play/quizzing/Scoreboard.svelte
+++ frontend/src/play/quizzing/Scoreboard.svelte
@@ -1,1 +1,93 @@
-This is the scoreboard.
+<script>
+	import Score from './Score.svelte';
+	import {apiUrl} from '../../stores.js';
+	import {onDestroy, createEventDispatcher} from 'svelte';
+
+	export let game;
+	export let players;
+
+	const dispatch = createEventDispatcher();
+
+	let apiUrlStore;
+	let scores;
+	let fullPlayers;
+	$: {
+		fullPlayers = players.map(player => {
+			if (scores !== undefined) {
+				player.score = scores[player.id];
+			}
+			return player;
+		});
+		fullPlayers.sort((a, b) => {
+			if (a.score > b.score) {
+				return -1;
+			} else if (a.score < b.score) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+	}
+	let state = {
+		scores: {
+			attempted: false,
+			successful: false
+		}
+	};
+
+	init();
+
+	function init() {
+		const unsubscribeApiUrl = apiUrl.subscribe(value => apiUrlStore = value);
+
+		onDestroy(() => {
+			unsubscribeApiUrl();
+		});
+
+		fetch(`${apiUrlStore}game/${game.id}/score`, {
+			method: 'POST',
+			mode: 'cors'
+		}).then(res => {
+			state.scores.attempted = true;
+			state.scores.successful = true;
+			return res.json();
+		}).then(data => {
+			return scores = data;
+		}).catch(_ => {
+			state.scores.attempted = true;
+		});
+	}
+
+	function returnHome() {
+		dispatch('returnHome');
+	}
+
+	function nextQuestion() {
+		dispatch('nextQuestion');
+	}
+</script>
+
+<div class="uk-grid uk-margin" uk-grid="">
+	<div class="uk-width-expand">
+		<h1>
+			Scoreboard
+		</h1>
+	</div>
+	<div class="uk-width-auto">
+		<button class="uk-button uk-button-default uk-border-rounded" on:click={returnHome}>Cancel and return home
+		</button>
+		<button class="uk-button uk-button-primary uk-border-rounded" on:click={nextQuestion}>Next question</button>
+	</div>
+</div>
+
+{#each fullPlayers as player, i}
+	<Score color={player.color} score={player.score} position={i+1}/>
+    {#if fullPlayers.length > i+1 && i+1 > 2}
+		<hr class="uk-margin-small">
+    {/if}
+{:else}
+	<div class="uk-card uk-card-default uk-card-small uk-border-rounded">
+		<div uk-spinner="ratio: 3"></div>
+		Fetching quizzes
+	</div>
+{/each}
