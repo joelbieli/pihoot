2bdd49e137768474c9bd50fed00d5506963a17c8
laniw <lani.wagner@edu.tbz.ch>
2020-01-19 15:56:00 +0100

    Updates colors to match backend

--- frontend/src/util/playUtils.js
+++ frontend/src/util/playUtils.js
@@ -1,28 +1,26 @@
 export function getHexForColor(color) {
     const colors = {
         RED: "#D01937",
-        ORANGE: "#D89D00",
         YELLOW: "#e3e846",
         GREEN: "#25880C",
+        LIGHTBLUE: "#3ab1bb",
         BLUE: "#1460BE",
-        PURPLE: "#7132B2",
-        MAGENTA: "#b324b3"
+        PURPLE: "#7132B2"
     };
+
     switch (color) {
         case 'RED':
             return colors.RED;
-        case 'ORANGE':
-            return colors.ORANGE;
         case 'YELLOW':
             return colors.YELLOW;
         case 'GREEN':
             return colors.GREEN;
+        case 'LIGHTBLUE':
+            return colors.LIGHTBLUE;
         case 'BLUE':
             return colors.BLUE;
         case 'PURPLE':
             return colors.PURPLE;
-        case 'MAGENTA':
-            return colors.MAGENTA;
         default:
             console.error('Unknown color!');
     }
@@ -36,17 +34,15 @@ export function getTextHexForColor(color) {
     switch (color) {
         case 'RED':
             return colors.WHITE;
-        case 'ORANGE':
-            return colors.WHITE;
         case 'YELLOW':
             return colors.BLACK;
         case 'GREEN':
             return colors.WHITE;
+        case 'LIGHTBLUE':
+            return colors.WHITE;
         case 'BLUE':
             return colors.WHITE;
         case 'PURPLE':
-            return colors.WHITE;
-        case 'MAGENTA':
             return colors.WHITE;
         default:
             console.error('Unknown color!');
